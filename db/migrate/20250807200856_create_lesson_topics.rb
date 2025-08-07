8b91146f32f5e17db3d347d330f727c67e48be0b
Brittany Halterman <halterma@gmail.com>
2025-08-07 16:18:26 -0400

    Add associations to topics and lessons

--- db/migrate/20250807200856_create_lesson_topics.rb
+++ db/migrate/20250807200856_create_lesson_topics.rb
@@ -1,6 +1,9 @@
 class CreateLessonTopics < ActiveRecord::Migration[8.0]
   def change
     create_table :lesson_topics do |t|
+      t.references :topics
+      t.references :lesson
+      
       t.timestamps
     end
   end
