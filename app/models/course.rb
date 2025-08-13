479291229f59d31645b4a53edae1fa380ec3af91
Brittany Halterman <halterma@gmail.com>
2025-08-13 11:25:48 -0400

    Use TDD to implement a method on the Course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,14 @@ class Course < ApplicationRecord
   belongs_to :trimester
   has_many :enrollments
 
+  def student_name_list
+    student_names = []
+    self.enrollments.each do |enrollment|
+      student_names << "#{enrollment.student.first_name} #{enrollment.student.last_name}"
+    end
+
+    student_names
+  end
+
   delegate :title, to: :coding_class
 end
