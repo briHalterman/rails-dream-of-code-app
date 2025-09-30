23e70df3ebaed7978c83399e8ea4190460ee4cd6
Brittany Halterman <halterma@gmail.com>
2025-09-30 10:35:39 -0400

    Implement initial login error handling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,15 @@ class SessionsController < ApplicationController # rubocop:disable Style/Documen
     # 3. If authentication is successful, set user info in a session
     # 4. If any of the above fail, return an error to the user and re-render the login form
     # 5. Look for a user with the username in the params (and re-render the form with an error if none is found)
+
+    user = User.find_by(username: params[:username])
+
+    if user
+      # Success
+    else
+      flash.now[:alert] = 'Invalid username or password.'
+      render :new
+    end
   end
 
   def destroy; end
