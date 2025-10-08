ac5d26b98d0a4eda35be080841ab2456918b7a59
Brittany Halterman <halterma@gmail.com>
2025-10-08 10:20:55 -0400

    Respond with static data

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
-class Api::V1::CoursesController < ApplicationController
-  def index
+module Api
+  module V1
+    class CoursesController < ApplicationController # rubocop:disable Style/Documentation
+      def index # rubocop:disable Metrics/MethodLength
+        courses_hash = {
+          courses: [
+            {
+              id: 55,
+              title: 'Intro to Programming',
+              application_deadline: '2025-01-15',
+              start_date: '2025-01-15'
+            },
+            {
+              id: 61,
+              title: 'Ruby on Rails',
+              application_deadline: '2025-01-15',
+              start_date: '2025-01-15',
+              end_date: '2025-01-15'
+            }
+          ]
+        }
+
+        render json: courses_hash, status: :ok
+      end
+    end
   end
 end
