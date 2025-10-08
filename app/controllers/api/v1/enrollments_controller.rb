613dc9f1824c9c8a62028adbed01f39e1a44e268
Brittany Halterman <halterma@gmail.com>
2025-10-08 14:35:57 -0400

    Create enrollments controller and index action

---
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class EnrollmentsController < ApplicationController # rubocop:disable Style/Documentation
+      def index; end
+    end
+  end
+end
