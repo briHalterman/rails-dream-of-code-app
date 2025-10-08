ff7392740df406e77d8effab35d2972b37846518
Brittany Halterman <halterma@gmail.com>
2025-10-08 14:48:54 -0400

    Respond with static data

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -3,7 +3,28 @@
 module Api
   module V1
     class EnrollmentsController < ApplicationController # rubocop:disable Style/Documentation
-      def index; end
+      def index # rubocop:disable Metrics/MethodLength
+        enrollments_hash = {
+          enrollments: [
+            {
+              "id": 278,
+              "studentId": 68,
+              "studentFirstName": 'Bob',
+              "studentLastName": 'Burger',
+              "finalGrade": ''
+            },
+            {
+              "id": 302,
+              "studentId": 89,
+              "studentFirstName": 'Marge',
+              "studentLastName": 'Simpson',
+              "finalGrade": ''
+            }
+          ]
+        }
+
+        render json: enrollments_hash, status: :ok
+      end
     end
   end
 end
